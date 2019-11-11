eb0d0cd1ab53e2b4c9d2d76052ff90079f9e9601
alexiliev <iliev.alex@outlook.com>
2019-11-11 09:21:05 -0800

    initial commit

---
+++ src/components/DisplayPosts.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { listPosts } from "../graphql/queries";
+import { API, graphqlOperation } from "aws-amplify";
+
+class DisplayPosts extends Component {
+    state = {
+        posts: []
+    }
+
+    componentDidMount = async() => {
+        this.getPosts()
+    }
+
+    getPosts = async() => {
+        const result = await API.graphql(graphqlOperation(listPosts))
+        // console.log('all posts: ', JSON.stringify(result.data.listPosts.items))
+        this.setState({posts: result.data.listPosts.items })
+    }
+
+    render() {
+
+        const {posts} = this.state;    
+
+        return posts.map((post) => {
+            return(
+                <div className="posts" key={ post.id }>
+                    <h1>
+                        {post.postTitle}
+                    </h1>
+                    <h2>
+                        {post.postBody}
+                    </h2>
+                    <span>
+                        {"wrote by:"} {post.postOwnerUsername}
+                        {" on "}
+                        <time>
+                            {" "}
+                            {new Date(post.createdAt).toDateString()}
+                        </time>
+                    </span>
+                </div>
+            )
+        })
+    }
+}
+
+export default DisplayPosts;
